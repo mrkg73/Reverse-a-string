fcfe73c8bc9fab5cba85b9ebcb4d6f6db0df23c7
mrkg73 <moh771973@gmail.com>
2025-11-29 14:41:36 -0500

    Reversing string using stream

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+
+
 public class Main
 {
     public static void main(String[] args)
@@ -9,16 +11,6 @@ public class Main
         String nam= sc.nextLine();
         sc.close();
 
-        char[] chr= nam.toCharArray();
-        int size=nam.length(), count =size-1;
-        char[] reversedString= new char[size];
-
-
-       for(int i=0;i<size;i++)
-        {
-            reversedString [i] =chr[count];
-            count-=1;
-        }
-        System.out.print("Revered string is: " + new String(reversedString));
+         nam.chars().mapToObj(s->(char)s).toList().reversed().forEach(System.out::print);
     }
 }
